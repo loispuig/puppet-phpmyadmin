ba887257fb605975f0352f467a9c1718d39c6f08
Loïs <lois.puig@kctus.fr>
2016-01-22 19:26:53 +0100

    Moved to STABLE branch

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,7 +2,7 @@ class phpmyadmin::package inherits phpmyadmin {
 	vcsrepo { '/var/www/phpmyadmin':
 		ensure => latest,
 		provider => git,
-		revision => 'master',
+		revision => 'STABLE',
 		source => 'https://github.com/phpmyadmin/phpmyadmin.git',
 	}
 }
