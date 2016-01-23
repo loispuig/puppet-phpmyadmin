88dc86907ab85e7d7c20d91d3911f58eb772969b
Loïs <lois.puig@kctus.fr>
2016-01-23 17:32:55 +0100

    Modified git cloning phpmyadmin repo in favor of curl download archive (faster)

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,8 +1,27 @@
 class phpmyadmin::package inherits phpmyadmin {
-	vcsrepo { '/var/www/phpmyadmin':
-		ensure => latest,
-		provider => git,
-		revision => 'STABLE',
-		source => 'https://github.com/phpmyadmin/phpmyadmin.git',
+	# exec { 'phpmyadmin-install':
+	# 	cwd => '/var/www',
+	# 	environment => ["COMPOSER_HOME=/usr/local/bin"],
+	# 	command => 'composer create-project phpmyadmin/phpmyadmin --working-dir=/var/www --repository-url=https://www.phpmyadmin.net/packages.json',
+	# 	path => ['/usr/local/bin', '/usr/bin', '/bin', '/usr/local/sbin', '/usr/sbin', '/sbin'],
+	# 	user => root,
+	# 	require => Exec['composer-install'],
+	# 	unless => 'test -d /var/www/phpmyadmin',
+	# 	timeout => 900,
+	# }
+
+	# vcsrepo { '/var/www/phpmyadmin':
+	# 	ensure => latest,
+	# 	provider => git,
+	# 	revision => 'master',
+	# 	source => 'https://github.com/phpmyadmin/phpmyadmin.git',
+	# }
+
+	exec { 'phpmyadmin-download':
+		cwd => '/tmp',
+		path => ['/usr/local/bin', '/usr/bin', '/bin', '/usr/local/sbin', '/usr/sbin', '/sbin'],
+		# command => 'curl -L -o phpmyadmin-stable.zip https://github.com/phpmyadmin/phpmyadmin/archive/STABLE.zip',
+		command => 'curl -LO https://github.com/phpmyadmin/phpmyadmin/archive/STABLE.zip ; unzip STABLE.zip; rm STABLE.zip; mv phpmyadmin-STABLE /var/www/phpmyadmin',
+		require => Package['curl', 'unzip'],
 	}
 }
